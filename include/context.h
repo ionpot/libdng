8e4fbfe9f31b7f216a8c917e4c6022bc6b7d967c
Emir Kotan <emirkotan@gmail.com>
2020-09-29 22:58:42 +0100

    context (initial)

--- include/context.h
+++ include/context.h
@@ -1,23 +1,32 @@
 #ifndef LIBDNG_CONTEXT_H
 #define LIBDNG_CONTEXT_H
 
+#include "combat.h"
+#include "dice-pool.h"
 #include "event.h"
+#include "grid.h"
+#include "mempool.h"
+#include "spell-slots.h"
 
-struct dngContext;
+struct dngContext {
+	struct dngCombat * combat;
+	struct dngDicePool dices;
+	struct dngEvent event;
+	struct dngGrid grid;
+	struct dngMemPool * mempool;
+	struct dngSpellSlots slots;
+};
 
 struct dngContext *
-dngContext_create();
+dngContext_create(void);
 
 void
 dngContext_destroy(struct dngContext *);
 
 enum dngEvent_Id
-dngContext_firstEvent(struct dngContext *);
-
-enum dngEvent_Id
 dngContext_nextEvent(struct dngContext *);
 
-const struct dngGrid *
-dngContext_getGrid(const struct dngContext *);
+void
+dngContext_reset(struct dngContext *);
 
 #endif
---
+++ src/context.c
@@ -0,0 +1,92 @@
+#include "context.h"
+
+#include "combat.h"
+#include "dice-pool.h"
+#include "event.h"
+#include "grid.h"
+#include "int.h"
+#include "mempool.h"
+#include "spell-slots.h"
+
+#include <assert.h>
+#include <stddef.h>
+
+typedef struct dngContext T;
+
+static T *
+create(struct dngMemPool * mem)
+{
+	assert(mem);
+	T * self = dngMemPool_alloc(mem, sizeof(T));
+	if (!self)
+		return NULL;
+	self->combat = dngCombat_create(mem);
+	if (!self->combat)
+		return NULL;
+	self->dices = dngDicePool_create(mem);
+	if (!self->dices.pool)
+		return NULL;
+	self->event = dngEvent_create();
+	dngGrid_clear(&self->grid);
+	self->mempool = mem;
+	self->slots = dngSpellSlots_create(mem);
+	if (!self->slots.nodes)
+		return NULL;
+	return self;
+}
+
+T *
+dngContext_create(void)
+{
+	size_t growth_size = dngInt_kib(1);
+	struct dngMemPool * mem =
+		dngMemPool_create(growth_size);
+	if (!mem)
+		return NULL;
+	T * self = create(mem);
+	if (!self)
+		dngMemPool_destroy(mem);
+	return self;
+}
+
+void
+dngContext_destroy(T * self)
+{
+	assert(self);
+	dngMemPool_destroy(self->mempool);
+}
+
+enum dngEvent_Id
+dngContext_nextEvent(T * self)
+{
+	assert(self);
+	enum dngEvent_Id id =
+		dngEvent_next(&self->event);
+	switch (id) {
+	case dngInput_ATTACK:
+		if (dngCombat_isTurnOfSide(self->combat, dngGrid_SIDE_A))
+			break;
+		goto next;
+	case dngOutput_COMBAT_END:
+		if (dngCombat_hasEnded(self->combat))
+			break;
+		dngEvent_jump(&self->event, dngOutput_COMBAT_BEGIN);
+		goto next;
+	case dngOutput_NEXT_ROUND:
+		if (dngCombat_isEndOfRound(self->combat))
+			break;
+		goto next;
+	default:
+		break;
+	}
+	return id;
+next:
+	return dngContext_nextEvent(self);
+}
+
+void
+dngContext_reset(T * self)
+{
+	assert(self);
+	self->event = dngEvent_create();
+}
