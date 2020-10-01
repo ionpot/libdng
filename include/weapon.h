2d997614126fd2e9302e2564951784d65ae27b99
Emir Kotan <emirkotan@gmail.com>
2020-10-01 06:32:45 +0100

    rm dice from weapon

--- include/weapon.h
+++ include/weapon.h
@@ -1,8 +1,6 @@
 #ifndef LIBDNG_WEAPON_H
 #define LIBDNG_WEAPON_H
 
-#include "dice.h"
-
 enum dngWeapon_Id {
 	dngWeapon_DAGGER,
 	dngWeapon_LONG_AXE,
@@ -12,8 +10,5 @@ enum dngWeapon_Id {
 
 int
 dngWeapon_getInitiative(enum dngWeapon_Id);
-
-struct dngDice
-dngWeapon_getDice(enum dngWeapon_Id);
 
 #endif
--- src/weapon.c
+++ src/weapon.c
@@ -1,10 +1,5 @@
 #include "weapon.h"
 
-#include "dice.h"
-
-#include <assert.h>
-#include <stdbool.h>
-
 int
 dngWeapon_getInitiative(enum dngWeapon_Id id)
 {
@@ -16,23 +11,5 @@ dngWeapon_getInitiative(enum dngWeapon_Id id)
 	case dngWeapon_STAFF:
 	case dngWeapon_SWORD:
 		return 0;
-	default:
-		assert(false);
-	}
-}
-
-struct dngDice
-dngWeapon_getDice(enum dngWeapon_Id id)
-{
-	switch (id) {
-	case dngWeapon_DAGGER:
-		return dngDice_d4;
-	case dngWeapon_LONG_AXE:
-		return dngDice_d8;
-	case dngWeapon_STAFF:
-	case dngWeapon_SWORD:
-		return dngDice_d6;
-	default:
-		assert(false);
 	}
 }
