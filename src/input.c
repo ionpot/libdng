dbd5af3d4dbabaac4175f84e0f46879ee7e42646
Emir Kotan <emirkotan@gmail.com>
2020-10-15 04:33:28 +0100

    input.c

--- src/input.c
+++ src/input.c
@@ -19,15 +19,16 @@ void
 dngInput_forEntity(struct dngContext * context, struct dngInput_NewEntity input)
 {
 	assert(context);
-	struct dngEntity * entity = dngEntities_next(&context->entities);
+	struct dngEntity * entity = dngEntities_next(context->entities);
 	if (!entity)
 		return;
-	*entity = dngEntity_fromInput((struct dngEntity_Input){
+	struct dngEntity_Input entity_input = {
 		.attr = input.attr,
 		.klass = input.klass,
 		.race = dngRace_HUMAN,
 		.slots = context->slots,
 		.weapon = input.weapon
-	});
+	};
+	*entity = dngEntity_fromInput(&entity_input);
 	dngGrid_putEntity(&context->grid, input.position, entity);
 }
